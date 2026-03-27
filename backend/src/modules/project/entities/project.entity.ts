76b18e7c37427e13f0c1d6a42a57d23301f87e91
admin <peancharoen.pslcp3@gmail.com>
2026-03-27 16:51:57 +0700

    690327:1651 Fixing Project list by ATG Opus #01

--- backend/src/modules/project/entities/project.entity.ts
+++ backend/src/modules/project/entities/project.entity.ts
@@ -1,7 +1,19 @@
-import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+// File: backend/src/modules/project/entities/project.entity.ts
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  OneToMany,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
 import { Exclude } from 'class-transformer';
 import { UuidBaseEntity } from '../../../common/entities/uuid-base.entity';
 import { Contract } from '../../contract/entities/contract.entity';
+
+// Change Log:
+// - 2026-03-27: เพิ่ม createdAt, updatedAt, deletedAt ที่ขาดหายไปตาม DB schema (แก้ Unknown column 'project.createdAt')
 
 @Entity('projects')
 export class Project extends UuidBaseEntity {
@@ -19,6 +31,15 @@ export class Project extends UuidBaseEntity {
 
   @Column({ name: 'is_active', default: 1, type: 'tinyint' })
   isActive!: boolean;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt!: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt!: Date;
+
+  @DeleteDateColumn({ name: 'deleted_at', select: false })
+  deletedAt?: Date;
 
   @OneToMany(() => Contract, (contract) => contract.project)
   contracts!: Contract[];
